2f54cd5d6c632ce1c291d8845f092192faeb3991
DimaMen265 <menzak87@gmail.com>
2024-01-25 14:51:42 +0100

    The end tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,50 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    public getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+    
+    public getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[];
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+
+    public openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
